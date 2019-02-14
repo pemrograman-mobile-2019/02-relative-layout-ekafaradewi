db90f89b3fbb68e8e153d1f52d9fc9291ce3cc98
ekafaradewi <44692821+ekafaradewi@users.noreply.github.com>
2019-02-14 14:30:13 +0700

    ekaRelative

---
+++ app/src/main/java/com/example/asusglobal/relativelayouteka/RelativeEka.java
@@ -0,0 +1,13 @@
+package com.example.asusglobal.relativelayouteka;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelativeEka extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_eka);
+    }
+}
